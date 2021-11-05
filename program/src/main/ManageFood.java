284fd30e0a1ed69755e50c65ecf8b2f2f1d53cf5
zixuan_sunnyday <zixuantroy.zhou@mail.utoronto.ca>
2021-11-05 17:38:26 -0400

    Read the food data by constructor in ManageFood

--- program/src/main/ManageFood.java
+++ program/src/main/ManageFood.java
@@ -1,11 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class ManageFood {
     HashMap<String, Food> FoodData;//store the name and corresponding food
 
-    public ManageFood(){
+    public ManageFood() throws FileNotFoundException {
         HashMap<String, Food> food_data = new HashMap<>();
+        File file = new File("/database/FoodData.txt");
+        Scanner scanner = new Scanner(file);
+        while(scanner.hasNextLine()){
+            String food_name = scanner.nextLine().strip();
+            String food_kind = scanner.nextLine().strip();
+            String food_weight = scanner.nextLine().strip();
+            String food_calorie = scanner.nextLine().strip();
+            Food new_food = new Food(food_name,
+                                    food_kind,
+                                    Double.parseDouble(food_weight),
+                                    Double.parseDouble(food_calorie));
+            food_data.put(food_name, new_food);
+            scanner.nextLine();
+        }
     }
     public void update_food(String food, String kind, double weight, double calo_per_gram) {
            Food fd = new Food(food, kind, weight, calo_per_gram);
