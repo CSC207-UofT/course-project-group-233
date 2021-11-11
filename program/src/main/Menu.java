bf4ce48da38e6999e419165fa4ea18894eed9689
Poppy-Hao <yinh4@miamioh.edu>
2021-11-11 16:57:19 -0500

    add search

--- program/src/main/Menu.java
+++ program/src/main/Menu.java
@@ -1,5 +1,6 @@
 import java.io.FileNotFoundException;
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class Menu {
@@ -17,7 +18,7 @@ public class Menu {
         this.mf = new ManageFood();
     }
 
-    public void show_given_food_calorie (String fd, Calendar date, double weight){
+    public void show_given_food_calorie(String fd, Calendar date, double weight) {
         //food calorie showed after clicking Food and entering food name in menu
         mf.change_food_weight(fd, weight);
         ma.update_food_record(date, fd, this.mf);
@@ -35,5 +36,23 @@ public class Menu {
 
     public ManageAccount getMa() {
         return this.ma;
+    }
+
+    public String searching(String food_name) {
+        ArrayList<String> food_recommend = new ArrayList<String>();
+        for (String food : this.mf.food_list()) {
+            if (food.equals(food_name)) {
+                return food_name;
+            } else {
+
+                for (String item : this.mf.food_list()) {
+                    if (item.charAt(0) == food_name.charAt(0)) {
+                        food_recommend.add(item);
+                    }
+                }
+            }
+        }
+        String recommend = food_recommend.toString();
+        return "The food is not founded. Do you mean:" + recommend;
     }
 }
