86d46d336a961e7bc56deef7e35ea4d2968c7d0a
Yumeng He <2684438857@qq.com>
2021-11-29 13:30:13 -0800

    initialize ManageExercise Class

--- program/src/main/ManageExercise.java
+++ program/src/main/ManageExercise.java
@@ -1,2 +1,22 @@
-//public class ManageExercise {
-//}
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ManageExercise {
+
+    public void ManageAccount(){}
+
+    public boolean findExercise(String name) throws FileNotFoundException {
+        File text = new File("ExerciseData.txt");
+        Scanner s = new Scanner(text);
+        boolean has_exercise=false;
+        String line=s.nextLine();
+        while(s.hasNextLine()) {
+            if (line.equals(name)) {
+                has_exercise = true;
+            } else {
+                line=s.nextLine();}
+        }
+        return has_exercise;
+    }
+}
